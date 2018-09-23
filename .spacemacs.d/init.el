b92dac5252be187412ff3bda87da486fbb9919dc
evgeniTUM <evgeniTUM@users.noreply.github.com>
2018-09-23 09:32:14 +0200

    deactivate exwm

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -88,7 +88,6 @@ values."
 
      ;; custom
      myConfig
-     exwm
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
