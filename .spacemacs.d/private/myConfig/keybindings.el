92da1d689440d4d3da695c28012cd27b8ba52a38
Evgeni Pavlidis <evgeniTUM@users.noreply.github.com>
2018-11-23 15:23:55 +0100

    shell specific changes

--- .spacemacs.d/private/myConfig/keybindings.el
+++ .spacemacs.d/private/myConfig/keybindings.el
@@ -1,6 +1,7 @@
 (spacemacs/set-leader-keys "b SPC" 'rename-buffer)
 (spacemacs/set-leader-keys "ie" 'eval-and-replace)
 (spacemacs/set-leader-keys "ao SPC" 'evgeni/open-my-calendar)
+(spacemacs/set-leader-keys "ass" 'counsel-switch-to-shell-buffer)
 
 (spacemacs/set-leader-keys "hM" 'helm-descbinds)
 (spacemacs/set-leader-keys "hm" 'which-key-show-major-mode)
@@ -13,4 +14,5 @@
 (spacemacs/set-leader-keys "o o" 'local-set-key)
 (spacemacs/set-leader-keys "o O" 'global-set-key)
 
- 
+;;; mode specific bindings
+(define-key shell-mode-map (kbd "C-M-r") 'counsel-shell-history)
